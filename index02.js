3c9a89c2ff34843ce31973242fb0fc70a2d8ff75
Vishal Modanwal <modanwalvishal@gmail.com>
2021-08-01 15:11:44 +0530

    userscoreabove85

---
+++ index02.js
@@ -0,0 +1,52 @@
+const users = [
+  {
+    name: "Brook",
+    scores: 75,
+    skills: ["HTM", "CSS", "JS"],
+    age: 16,
+  },
+  {
+    name: "Alex",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "David",
+    scores: 75,
+    skills: ["HTM", "CSS"],
+    age: 22,
+  },
+  {
+    name: "John",
+    scores: 85,
+    skills: ["HTM"],
+    age: 25,
+  },
+  {
+    name: "Sara",
+    scores: 95,
+    skills: ["HTM", "CSS", "JS"],
+    age: 26,
+  },
+  {
+    name: "Martha",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "Thomas",
+    scores: 90,
+    skills: ["HTM", "CSS", "JS"],
+    age: 20,
+  },
+];
+
+//function to filter out users who scores above 85;
+const scoresGreaterThan85 = (users) => {
+  const userscoresabove85 = users.filter((elem, index) => elem.scores > 85);
+  console.log(userscoresabove85);
+};
+
+scoresGreaterThan85(users);
